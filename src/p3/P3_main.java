cc73bd42949b6d5bf85c728d7edd89b720544242
tsanewww12 <pernalievgosho@gmail.com>
2025-11-06 13:13:55 +0100

    Update project

--- src/p3/P3_main.java
+++ src/p3/P3_main.java
@@ -3,10 +3,10 @@ package p3;
 public class P3_main {
 
 	public static void main(String[] args) {
-		double a  =4;
+		double a  =7;
 		System.out.println("Deine Zahl ist " + a);
-		double b = a*a;
-		System.out.println("Die Quadrat deiner Zahl ist " + b );
+		
+		System.out.println("Die Quadrat deiner Zahl ist " + a*a );
 		if (a%2==0)
 		{
 			System.out.println("Deine Zahl is gerade");
